3f7d7aae6cbb27e3cfd0580b2dc2ebc1b8b1864a
fabiano.bfcarvalho <fabiano.bfcarvalho@CAS555509W0710.senacsp.edu.br>
2017-03-03 21:17:58 -0300

    Criação da classe contato.

---
+++ src/main/java/br/senac/tads/pi3a/model/Contato.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3a.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author fabiano.bfcarvalho
+ */
+public class Contato {
+    private String nome;
+    private Date dataNasc;
+    private int telefone;
+    private String email;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the dataNasc
+     */
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    /**
+     * @param dataNasc the dataNasc to set
+     */
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public int getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean addContato(){
+        
+        return true;
+    }
+    public boolean removerContato(){
+        
+        return true;
+    }
+    
+}
