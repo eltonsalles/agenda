8ab3bf9fccb36ec87eafcef34fefeecd01ea9b3a
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-03-04 12:05:11 -0300

    Correção de campos no banco de dados

--- src/main/java/resources/SQL.sql
+++ src/main/java/resources/SQL.sql
@@ -5,14 +5,14 @@
  * Criar o banco de dados agenda 
  * com o usuário agenda_u e a senha agenda_p
  *
- * Depois rodar estes scripts para a criação das tabelas
+ * Depois rodar estes scripts para a criação da tabela
  */
 
 CREATE TABLE contato (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
     nome VARCHAR(150) NOT NULL,
-    data_nasc DATE,
+    data_nasc DATE NOT NULL,
     email VARCHAR(255) NOT NULL,
     telefone VARCHAR(16) NOT NULL,
-    data_cadastro TIMESTAMP,
+    data_cadastro TIMESTAMP NOT NULL,
 );
