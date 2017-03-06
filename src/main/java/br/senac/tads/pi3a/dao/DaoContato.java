038888c7e066a7905deae8d32a9dc25ab41a2daf
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-03-06 11:34:00 -0300

    Colocado os comentários nos métodos, retirado métodos não usados e formatado a classe

--- src/main/java/br/senac/tads/pi3a/dao/DaoContato.java
+++ src/main/java/br/senac/tads/pi3a/dao/DaoContato.java
@@ -16,6 +16,14 @@ import br.senac.tads.pi3a.utils.Conexao;
  */
 public class DaoContato {
 
+    /**
+     * Faz insert na tabela contato
+     *
+     * @param contato
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     public static boolean inserir(Contato contato) throws SQLException, Exception {
         String sql;
 
@@ -27,6 +35,14 @@ public class DaoContato {
         return true;
     }
 
+    /**
+     * Faz update na tabela contato
+     *
+     * @param contato
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     public static boolean alterar(Contato contato) throws SQLException, Exception {
         String sql;
 
@@ -38,6 +54,14 @@ public class DaoContato {
         return true;
     }
 
+    /**
+     * Exclui um registro pelo id na tabela contato
+     *
+     * @param id
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     public static boolean excluir(int id) throws SQLException, Exception {
         String sql;
 
@@ -48,19 +72,14 @@ public class DaoContato {
         return true;
     }
 
-    public static List<Contato> consultarContatos()
-            throws SQLException, Exception {
-
-        List<Contato> listaContato;
-        String sql;
-
-        sql = "SELECT * FROM contato";
-
-        listaContato = executarConsulta(sql);
-
-        return listaContato;
-    }
-
+    /**
+     * Faz uma consulta na tabela contato pelo id
+     *
+     * @param id
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     public static Contato consultarContatoPorId(int id)
             throws SQLException, Exception {
         List<Contato> listaContato;
@@ -77,6 +96,14 @@ public class DaoContato {
         return null;
     }
 
+    /**
+     * Faz uma consulta na tabela contato pelo nome
+     *
+     * @param nome
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     public static List<Contato> consultarContatoPorNome(String nome)
             throws SQLException, Exception {
         List<Contato> listaContato;
@@ -93,7 +120,16 @@ public class DaoContato {
         return null;
     }
 
-    private static void executarSql(String sql, Contato contato, 
+    /**
+     * Executa operações de insert ou update
+     *
+     * @param sql
+     * @param contato
+     * @param operacao
+     * @throws SQLException
+     * @throws Exception
+     */
+    private static void executarSql(String sql, Contato contato,
             String operacao) throws SQLException, Exception {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -106,7 +142,7 @@ public class DaoContato {
             stmt.setDate(2, new Date(contato.getDataNasc().getTime()));
             stmt.setString(3, contato.getEmail());
             stmt.setString(4, contato.getTelefone());
-            
+
             if (operacao.equalsIgnoreCase("insert")) {
                 stmt.setDate(5, new Date(contato.getDataCadastro().getTime()));
             } else {
@@ -125,7 +161,15 @@ public class DaoContato {
         }
     }
 
-    private static void executarSql(String sql, int id) 
+    /**
+     * Executa a operação delete no banco de dados
+     *
+     * @param sql
+     * @param id
+     * @throws SQLException
+     * @throws Exception
+     */
+    private static void executarSql(String sql, int id)
             throws SQLException, Exception {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -148,42 +192,16 @@ public class DaoContato {
         }
     }
 
-    private static List<Contato> executarConsulta(String sql)
-            throws SQLException, Exception {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet result;
-        List<Contato> listaContato = new ArrayList<>();
-
-        try {
-            conn = Conexao.getConexao();
-            stmt = conn.prepareStatement(sql);
-
-            result = stmt.executeQuery();
-
-            while (result.next()) {
-                Contato contato = new Contato(
-                        result.getString("nome"),
-                        result.getDate("data_nasc"),
-                        result.getString("telefone"),
-                        result.getString("email"));
-
-                listaContato.add(contato);
-            }
-        } finally {
-            if (stmt != null && !stmt.isClosed()) {
-                stmt.close();
-            }
-
-            if (conn != null && !conn.isClosed()) {
-                conn.isClosed();
-            }
-        }
-
-        return listaContato;
-    }
-
-    private static List<Contato> executarConsulta(String sql, String valor) 
+    /**
+     * Faz a operação select no banco de dados usando como paramêtro uma string
+     *
+     * @param sql
+     * @param valor
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
+    private static List<Contato> executarConsulta(String sql, String valor)
             throws SQLException, Exception {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -221,6 +239,15 @@ public class DaoContato {
         return listaContato;
     }
 
+    /**
+     * Faz a operação select no banco de dados usando como paramêtro um int
+     *
+     * @param sql
+     * @param valor
+     * @return
+     * @throws SQLException
+     * @throws Exception
+     */
     private static List<Contato> executarConsulta(String sql, int valor)
             throws SQLException, Exception {
         Connection conn = null;
