fef8d771aa040218d137740d3a1d9fb85bee8d64
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-03-06 11:41:01 -0300

    Colocado o SQLException nos métodos e retirado os métodos desnecessários

--- src/main/java/br/senac/tads/pi3a/service/ServicoContato.java
+++ src/main/java/br/senac/tads/pi3a/service/ServicoContato.java
@@ -2,6 +2,7 @@ package br.senac.tads.pi3a.service;
 
 import br.senac.tads.pi3a.model.Contato;
 import br.senac.tads.pi3a.dao.DaoContato;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -16,11 +17,15 @@ public class ServicoContato {
      * 
      * @param contato
      * @return
+     * @throws SQLException
      * @throws Exception 
      */
-    public static boolean cadastrar(Contato contato) throws Exception {
+    public static boolean cadastrar(Contato contato) 
+            throws SQLException, Exception {
         try {
             return DaoContato.inserir(contato);
+        } catch (SQLException ex) {
+            throw new SQLException(ex.getMessage());
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
@@ -32,11 +37,15 @@ public class ServicoContato {
      * 
      * @param contato
      * @return
+     * @throws SQLException
      * @throws Exception 
      */
-    public static boolean alterar(Contato contato) throws Exception {
+    public static boolean alterar(Contato contato) 
+            throws SQLException, Exception {
         try {
             return DaoContato.alterar(contato);
+        } catch (SQLException ex) {
+            throw new SQLException(ex.getMessage());
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
@@ -47,9 +56,10 @@ public class ServicoContato {
      * 
      * @param id
      * @return
+     * @throws SQLException
      * @throws Exception 
      */
-    public static boolean excluir(int id) throws Exception {
+    public static boolean excluir(int id) throws SQLException, Exception {
         try {
             if (id <= 0) {
                 throw new Exception("Informe um número válido de Id.");
@@ -57,20 +67,8 @@ public class ServicoContato {
             
             return DaoContato.excluir(id);
             
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-    
-    /**
-     * Retorna todos os contatos do banco de dados
-     * 
-     * @return
-     * @throws Exception 
-     */
-    public static List<Contato> consultarContatos() throws Exception {
-        try {
-            return DaoContato.consultarContatos();
+        } catch (SQLException ex) {
+            throw new SQLException(ex.getMessage());
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
@@ -81,15 +79,19 @@ public class ServicoContato {
      * 
      * @param id
      * @return
+     * @throws SQLException
      * @throws Exception 
      */
-    public static Contato consultarContatoPorId(int id) throws Exception {
+    public static Contato consultarContatoPorId(int id) 
+            throws SQLException, Exception {
         try {
             if (id <= 0) {
                 throw new Exception("Informe um número válido de Id.");
             }
             
             return DaoContato.consultarContatoPorId(id);
+        } catch (SQLException ex) {
+            throw new SQLException(ex.getMessage());
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
@@ -101,16 +103,19 @@ public class ServicoContato {
      * 
      * @param nome
      * @return
+     * @throws SQLException
      * @throws Exception 
      */
     public static List<Contato> consultarContatosPorNome(String nome) 
-            throws Exception {
+            throws SQLException, Exception {
         try {
             if (nome.isEmpty()) {
                 throw new Exception("Informe um nome para a pesquisa.");
             }
             
             return DaoContato.consultarContatoPorNome(nome);
+        } catch (SQLException ex) {
+            throw new SQLException(ex.getMessage());
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
